7011cefdb279d3454e9b325a0927b6e6b804eb5b
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2019-10-09 03:01:56 +0000

    Hello world Lambda function that takes one param

--- 5. AWS Lambda/Hello World/lambda_function.rb
+++ 5. AWS Lambda/Hello World/lambda_function.rb
@@ -1,12 +1,16 @@
 require 'json'
 
 def lambda_handler(event:, context:)
-    # TODO implement
+    method = event['httpMethod']
+    query_string = event['queryStringParameters'] || {}
+    name = query_string['name'] || 'World'
     {
         statusCode: 200,
         body: JSON.generate({
             code: 42,
-            message: 'Hello from Lambda!'
+            message: "Hello, #{name}!",
+            method: method,
+            memory: context.memory_limit_in_mb
         })
     }
 end
