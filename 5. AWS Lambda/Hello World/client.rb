fa9cd09cca7be7e657162427922ba5f276a519fa
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2019-10-23 02:57:41 +0000

    Added query string params to request

--- 5. AWS Lambda/Hello World/client.rb
+++ 5. AWS Lambda/Hello World/client.rb
@@ -7,12 +7,15 @@ API_KEY = 'p8sqXjf6rr8TlgYbmgTv59hzspTFyx1dySZCSpF3'
 connection = Faraday.new(url: URL)
 response = connection.get do |request|
   request.headers['x-api-key'] = API_KEY
+  request.params['name'] = 'Joker'
 end
 
 if response.success?
   puts response.status
   data = JSON.parse(response.body)
   puts data
-  puts data['code']
-  puts data['message']
+  p data['code']
+  p data['message']
+  p data['method']
+  p data['memory'].to_i
 end
